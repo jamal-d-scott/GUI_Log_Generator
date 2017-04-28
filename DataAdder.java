87882bc47e0389aaac47ff410e9dfcfb30273cff
Jamal Scott <jamaldscott@gmail.com>
2017-04-28 08:12:40 -0400

    Update DataAdder.java

--- DataAdder.java
+++ DataAdder.java
@@ -1,27 +1,34 @@
 import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JProgressBar;
-import javax.swing.JTextArea;
-import javax.swing.SpringLayout;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import java.awt.event.ActionListener;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
-public class DataAdder 
+
+public class adminPage 
 {
-	private JFrame data;
-	private JTextField partnerNameField, partnerNumberField,userKeys, userRadio, userFlashlight,partnerKeys, partnerRadio, partnerFlashlight, assignmentField;
-	public static String getPName = "Partner Name: ", getPNumber = "Partner Spo Number: ";
-	private DateFormat timeFormat = new SimpleDateFormat("HH:mm");
-	private Date time = new Date();
+
+	private JFrame frame;
+	private static Object rowData[][] = new Object [20][3];
+	private static Object columnNames[] = { "First:", "Last:", "SPO Number:"};
+	private static JTable table = new JTable(rowData, columnNames);
+	private static String accounts = "";
+
 	/**
 	 * Launch the application.
 	 */
-	public static void dataWin() 
+	public static void administration() 
 	{
 		EventQueue.invokeLater(new Runnable() 
 		{
@@ -29,10 +36,9 @@ public class DataAdder
 			{
 				try 
 				{
-					DataAdder window = new DataAdder();
-					window.data.setVisible(true);
-				} 
-				catch (Exception e)
+					adminPage window = new adminPage();
+					window.frame.setVisible(true);
+				} catch (Exception e) 
 				{
 					e.printStackTrace();
 				}
@@ -43,7 +49,7 @@ public class DataAdder
 	/**
 	 * Create the application.
 	 */
-	public DataAdder()
+	public adminPage() 
 	{
 		initialize();
 	}
@@ -51,154 +57,150 @@ public class DataAdder
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	public void initialize() 
+	private void initialize() 
 	{
-		data = new JFrame();
-		data.setBounds(100, 100, 353, 533);
-		data.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		data.setResizable(true);
-		data.getContentPane().setLayout(null);
-		populateFrame();
+		for(int i = 0; i < 2; i++)
+		{	
+	        if (i == 0 || i == 1) 
+	        	table.getColumnModel().getColumn(i).setPreferredWidth(200); 
+	        else 
+	        	table.getColumnModel().getColumn(i).setPreferredWidth(300); 
+		}
 		
-		JButton logAdditionalInfo = new JButton("Log Info");
-		logAdditionalInfo.addActionListener(new ActionListener() 
+		populateTable();
+			
+		frame = new JFrame();
+		frame.setBounds(100, 100, 698, 494);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblRegistered = new JLabel("Registered Users");
+		lblRegistered.setBounds(21, 21, 122, 14);
+		frame.getContentPane().add(lblRegistered);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setLocation(21, 45);
+		scrollPane.setSize(630, 348);
+		scrollPane.setViewportView(table);
+		frame.getContentPane().add(scrollPane);
+		
+		JButton btnAddUser = new JButton("Add User");
+		btnAddUser.addActionListener(new ActionListener()
 		{
-			public void actionPerformed(ActionEvent e) 
+			public void actionPerformed(ActionEvent arg0) 
 			{
-				AppendToLog();
-				data.dispose();
+				addUser();
 			}
 		});
-		logAdditionalInfo.setBounds(43, 457, 89, 23);
-		data.getContentPane().add(logAdditionalInfo);
+		btnAddUser.setBounds(113, 410, 100, 23);
+		frame.getContentPane().add(btnAddUser);
+		
+		JButton btnDeleteUser = new JButton("Delete User");
+		btnDeleteUser.addActionListener(new ActionListener() 
+		{
+			//TODO DELETE USER.SPOFILE***********
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				int row = table.getSelectedRow();
+				String toDelete =  table.getModel().getValueAt(row, 0).toString();
+				toDelete +=  table.getModel().getValueAt(row, 1).toString();
+				toDelete +=  table.getModel().getValueAt(row, 2).toString();
+				accounts = accounts.replace(toDelete,"");
+				
+				try
+				{
+					FileWriter out = new FileWriter(System.getProperty("user.dir")+"\\VerifiedUsers\\Validated Users.vuf",false);
+					out.write(accounts);
+					out.close();
+					frame.dispose();
+					JOptionPane.showMessageDialog(frame, "User entry :"+toDelete+" has been deleted.");
+					administration();
+				}
+				catch(IOException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		btnDeleteUser.setBounds(223, 410, 108, 23);
+		frame.getContentPane().add(btnDeleteUser);
 		
 		JButton btnCancel = new JButton("Cancel");
 		btnCancel.addActionListener(new ActionListener() 
 		{
-			public void actionPerformed(ActionEvent arg0)
+			public void actionPerformed(ActionEvent e) 
 			{
-				System.exit(0);
+				frame.dispose();
 			}
 		});
-		btnCancel.setBounds(206, 457, 89, 23);
-		data.getContentPane().add(btnCancel);
+		btnCancel.setBounds(462, 410, 112, 23);
+		frame.getContentPane().add(btnCancel);
+		
+		JButton btnResetPassword = new JButton("Reset Password");
+		btnResetPassword.setBounds(340, 410, 112, 23);
+		frame.getContentPane().add(btnResetPassword);
 	}
 	
-	void AppendToLog()
-	{	
-		Log.to = timeFormat.format(time);
-		Log.table.setValueAt("X",0,0);
-		Log.table.setValueAt(Log.to,0,1);
-		Log.table.setValueAt("Office",0,2);
-		
-		//Appends the Log writter's information first
-		Log.logField.append("\n"+FrontEnd.SPOInformation[2]+" "+FrontEnd.SPOInformation[3]+"\n"+FrontEnd.SPOInformation[4]+"\nIn-time: "+timeFormat.format(time));
-		
-		if(partnerNameField.getText().equals(""))
-		{	
-			Log.logField.append("\n\nPartner Name: "+"\nPartner SPO Number: "+"\nIn-time: ");
-			Log.logField.append("\n\n"+FrontEnd.SPOInformation[2]+ " "+FrontEnd.SPOInformation[3]+"'s equipment:"+"\nKeys: "+userKeys.getText()+"\t"+"Radio: "+userRadio.getText()+"\t"+"Flashlight: "+userFlashlight.getText());
-			Log.logField.append("\n\nPartner equipment:"+"\nKeys: "+partnerKeys.getText()+"\t"+"Radio: "+partnerRadio.getText()+"\t"+"Flashlight: "+partnerFlashlight.getText());
-		}
-		else
-		{
-			Log.logField.append("\n\n"+partnerNameField.getText() + "\nSPO"+partnerNumberField.getText()+"\nIn-time: "+timeFormat.format(time));
-			Log.logField.append("\n\n"+FrontEnd.SPOInformation[2]+ " "+FrontEnd.SPOInformation[3]+"'s equipment:"+"\nKeys: "+userKeys.getText()+"\t"+"Radio: "+userRadio.getText()+"\t"+"Flashlight: "+userFlashlight.getText());
-			Log.logField.append("\n\n"+partnerNameField.getText()+"'s equipment:"+"\nKeys: "+partnerKeys.getText()+"\t"+"Radio: "+partnerRadio.getText()+"\t"+"Flashlight: "+partnerFlashlight.getText());
-		}
-		Log.logField.append("\n\nPatrol Assignment: "+assignmentField.getText());
-	}
-	
-	//Adds all of the GUI elements to the frame
-	void populateFrame()
+	private void addUser()
 	{
-		partnerNameField = new JTextField();
-		partnerNameField.setBounds(140, 202, 166, 20);
-		data.getContentPane().add(partnerNameField);
-		partnerNameField.setColumns(10);
+		try
+		{
+			FileWriter out = new FileWriter(System.getProperty("user.dir")+"\\VerifiedUsers\\Validated Users.vuf",true); //the true will append the new data
+			String first= JOptionPane.showInputDialog(frame, "What is the user's first name?");
+			String last = JOptionPane.showInputDialog(frame, "What is the user's last name?");
+			String number = JOptionPane.showInputDialog(frame, "What is the user's SPO number?");
+			out.write(first +" "+last+" SPO"+number+"\n");
+			out.close();
+			populateTable();
+		}
+		catch(IOException ioe){}
+	}
+	
+	private void populateTable()
+	{
+		BufferedReader in;
+		String data = "", account = "";
+		int currentUser = 0, location = 0;
+		char c;
 		
-		partnerNumberField = new JTextField();
-		partnerNumberField.setBounds(140, 239, 166, 20);
-		data.getContentPane().add(partnerNumberField);
-		partnerNumberField.setColumns(10);
-		
-		JLabel lblpartnerName = new JLabel("Partner name:");
-		lblpartnerName.setBounds(20, 191, 110, 14);
-		data.getContentPane().add(lblpartnerName);
-		
-		JLabel lblpartnerNumber = new JLabel("Partner number:");
-		lblpartnerNumber.setBounds(20, 241, 120, 14);
-		data.getContentPane().add(lblpartnerNumber);
-		
-		JLabel lblEnterAdditionalInformation = new JLabel("Enter Additional information here:");
-		lblEnterAdditionalInformation.setBounds(83, 11, 249, 14);
-		data.getContentPane().add(lblEnterAdditionalInformation);
-		
-		JLabel userLable = new JLabel("Your keys:");
-		userLable.setBounds(20, 71, 96, 14);
-		data.getContentPane().add(userLable);
-		
-		JLabel lblnameRadioHere = new JLabel("Your radio:");
-		lblnameRadioHere.setBounds(20, 108, 109, 14);
-		data.getContentPane().add(lblnameRadioHere);
-		
-		JLabel lblnameFlashlightHere = new JLabel("Your flashlight:");
-		lblnameFlashlightHere.setBounds(20, 145, 109, 14);
-		data.getContentPane().add(lblnameFlashlightHere);
-		
-		userKeys = new JTextField();
-		userKeys.setBounds(140, 68, 166, 20);
-		data.getContentPane().add(userKeys);
-		userKeys.setColumns(10);
-		
-		userRadio = new JTextField();
-		userRadio.setBounds(139, 105, 167, 20);
-		data.getContentPane().add(userRadio);
-		userRadio.setColumns(10);
-		
-		userFlashlight = new JTextField();
-		userFlashlight.setBounds(140, 142, 166, 20);
-		data.getContentPane().add(userFlashlight);
-		userFlashlight.setColumns(10);
-		
-		JLabel lblpartnerKeys = new JLabel("Partner keys:");
-		lblpartnerKeys.setBounds(20, 279, 109, 14);
-		data.getContentPane().add(lblpartnerKeys);
-		
-		JLabel lblpartnerRadio = new JLabel("Partner radio:");
-		lblpartnerRadio.setBounds(20, 316, 109, 14);
-		data.getContentPane().add(lblpartnerRadio);
-		
-		JLabel lblNewLabel = new JLabel("Partner flashlight:");
-		lblNewLabel.setBounds(20, 354, 109, 14);
-		data.getContentPane().add(lblNewLabel);
-		
-		partnerKeys = new JTextField();
-		partnerKeys.setBounds(140, 276, 166, 20);
-		data.getContentPane().add(partnerKeys);
-		partnerKeys.setColumns(10);
-		
-		partnerRadio = new JTextField();
-		partnerRadio.setBounds(140, 313, 166, 20);
-		data.getContentPane().add(partnerRadio);
-		partnerRadio.setColumns(10);
-		
-		partnerFlashlight = new JTextField();
-		partnerFlashlight.setBounds(140, 350, 166, 20);
-		data.getContentPane().add(partnerFlashlight);
-		partnerFlashlight.setColumns(10);
-		
-		JLabel lblPatrolAssignment = new JLabel("Patrol Assignment:");
-		lblPatrolAssignment.setBounds(20, 414, 109, 14);
-		data.getContentPane().add(lblPatrolAssignment);
-		
-		assignmentField = new JTextField();
-		assignmentField.setBounds(140, 411, 166, 20);
-		data.getContentPane().add(assignmentField);
-		assignmentField.setColumns(10);
-		
-		JLabel lblFirstLast = new JLabel("(First & Last Name) ");
-		lblFirstLast.setBounds(20, 205, 110, 14);
-		data.getContentPane().add(lblFirstLast);
+		try
+		{
+			File file = new File(System.getProperty("user.dir")+"\\VerifiedUsers\\Validated Users.vuf");
+			if(file.exists() == false)
+			{
+				return;
+			}
+			in = new BufferedReader(new FileReader(file));
+			while(( c = (char)in.read() ) != (char)-1 )
+			{
+				account += c;
+			}
+			
+			if(account.contains("#Open-Beta/ALL"));
+				account = account.replace("#Open-Beta/ALL\n","");
+			accounts = account;
+			
+			for(int i = 0; i < account.length(); i++)
+			{
+				c = account.charAt(i);
+				data+=c;
+				
+				if(c == ' ')
+				{
+					table.setValueAt(data,currentUser,location);
+					location++;
+					data = "";
+				}
+				if(c == '\n' || c == '\r')
+				{
+					table.setValueAt(data,currentUser,location);
+					data = "";
+					currentUser++;
+					location = 0;
+				}
+			}
+		}
+		catch(IOException ioe){}
 	}
 }
